f01ee29cf76c0ce7c1a8823db911d763b3ae53de
Xiaorui Wang <105865593+XiaoruiWang-SH@users.noreply.github.com>
2025-03-24 10:56:25 +0100

    First Commit

---
+++ src/main/java/com/example/MyApplication.java
@@ -0,0 +1,32 @@
+/*
+ * @Author: Xiaorui Wang
+ * @Email: xiaorui.wang@usi.ch
+ * @Date: 2025-03-24 10:46:57
+ * @LastEditors: Xiaorui Wang
+ * @LastEditTime: 2025-03-24 10:48:22
+ * @Description: 
+ * Copyright (c) 2025 by Xiaorui Wang, All Rights Reserved. 
+ */
+
+
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class MyApplication {
+
+	@RequestMapping("/")
+	String home() {
+		return "Hello World!!!";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyApplication.class, args);
+	}
+
+}
