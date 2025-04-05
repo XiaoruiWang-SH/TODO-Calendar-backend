5f0623f9141cf21a19fcd90f87caf8a510f471ac
Xiaorui Wang <xiaorui.wang@usi.ch>
2025-04-05 09:02:25 +0000

    Refactor Docker Compose files for development and production environments to use environment-specific volume names for MySQL data. Update run script to ensure consistent command formatting and include author metadata.

--- run.sh
+++ run.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+###
+ # @Author: Xiaorui Wang
+ # @Email: xiaorui.wang@usi.ch
+ # @Date: 2025-04-04 08:45:09
+ # @LastEditors: Xiaorui Wang
+ # @LastEditTime: 2025-04-05 08:44:56
+ # @Description: 
+ # Copyright (c) 2025 by Xiaorui Wang, All Rights Reserved. 
+### 
 
 # Default to dev environment if not specified
 ENV=${1:-dev}
@@ -15,11 +24,11 @@ echo "Starting application in $ENV environment..."
 
 case "$ENV" in
   dev)
-    docker compose --project-name myapp_dev -f docker-compose-dev.yml  down
+    docker compose --project-name myapp_dev -f docker-compose-dev.yml down
     docker compose --project-name myapp_dev -f docker-compose-dev.yml --env-file .env.dev up --build -d
     ;;
   prod)
-    docker compose --project-name myapp_prod -f docker-compose-prod.yml  down
+    docker compose --project-name myapp_prod -f docker-compose-prod.yml down
     docker compose --project-name myapp_prod -f docker-compose-prod.yml --env-file .env.prod up --build -d
     ;;
 esac
